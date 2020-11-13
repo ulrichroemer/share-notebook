bb51f6253504bb0aa844aa4cdd2dd000b515f57b
Susanna Baars <s.baars@tu-braunschweig.de>
2020-11-13 18:44:47 +0100

    scipy and numpy removed

--- external_functions_MC_airplanes.py
+++ external_functions_MC_airplanes.py
@@ -1,21 +1,33 @@
+from math import *
+from random import gauss
 import matplotlib.pyplot as plt
-import numpy as np
-import scipy.stats as stats
 from matplotlib.patches import Arc
 
 
+def linspace(lo, hi, n):
+    x = [(hi - lo) / n * i + lo for i in range(n)]
+    
+    return x
+
+
+def norm_pdf(x, mu, sigma):
+    f = []
+    for i in range(100):
+        f.append(1/sqrt(2 * pi * sigma**2) * exp(-(x[i] - mu)**2 / (2 * sigma**2)))
+    
+    return f
+
+
 def plot_crew_distribution(mu, sigma):
-    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
-    plt.plot(x, stats.norm.pdf(x, mu, sigma))
+    x = linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
+    plt.plot(x, norm_pdf(x, mu, sigma))
     plt.xlabel('$ m_{Crew} $ in [kg]')
     plt.ylabel('Wahrscheinlichkeit')
     plt.show()
 
 
 def visualization_MC_airplanes():
-    r = np.linspace(2.5, 5, 8)
-    step = np.zeros(10)
-
+    r = linspace(2.5, 5, 8)
     fig = plt.figure()
     ax = fig.gca()
     earth = plt.Circle((0, 0), 1, color='deepskyblue')
@@ -23,14 +35,14 @@ def visualization_MC_airplanes():
     erfolg_visulaisierung = 0
 
     for i in range(8):
-        rand = 370 + 20 * np.random.randn()
+        rand = gauss(mu=370, sigma=20)
         rand_arc = rand
         if rand > 360:
             erfolg_visulaisierung += 1
             rand_arc = 360
         flight = Arc((0, 0), r[i], r[i], angle=90.0, theta1=0.0, theta2=rand_arc)
         ax.add_artist(flight)
-        plt.scatter(r[i] / 2 * np.sin(- np.deg2rad(rand)), r[i] / 2 * np.cos(- np.deg2rad(rand)), marker="<",
+        plt.scatter(r[i] / 2 * sin(- radians(rand)), r[i] / 2 * cos(- radians(rand)), marker="<",
                     edgecolors='k', zorder=10)
 
     ax.add_artist(earth)
